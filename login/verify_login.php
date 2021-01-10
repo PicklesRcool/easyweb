27835227ea22a4ee3e02a7d32515c22eb71a317b
Iryna <iramykytyn@gmail.com>
2021-01-10 15:53:45 +0200

    Added proper folder structure

--- login/verify_login.php
+++ login/verify_login.php
@@ -1,17 +1,23 @@
 <?php  // verify_login.php 
 
+header("Location: ../task-choise/task-choise.html");
+
 include '../db_utils.php';
 
-$username = $_REQUEST['name'];
-$userpass = $_REQUEST['pass'];
+header("Location: ../task-choise/task-choise.html");
 
 $conn = DbOpenCon($secret_login, $secret_passw);
 
+header("Location: ../task-choise/task-choise.html");
+
 if (VerifyLogin($conn, $username, $userpass)) {
-  header("Location: ../task-choise");
+  header("Location: ../task-choise/task-choise.html");
 }
 else {
-  header("Location: login_again.html");
+  header("Location: login.html");
 }
+
+$username = $_REQUEST['name'];
+$userpass = $_REQUEST['pass'];
 
 ?>
